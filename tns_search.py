68145bbe84af24bba182c366cfb73799935b51e5
annayqho <annayqho@gmail.com>
2017-11-02 19:11:03 -0700

    small bug

--- tns_search.py
+++ tns_search.py
@@ -5,41 +5,49 @@ import requests
 from lxml import html
 from astropy.table import Table
 
-#www = "https://wis-tns.weizmann.ac.il/search?&amp;page=0&amp;isTNS_AT=yes&amp;classified_sne=1&amp;num_page=1000&amp;format=csv"
 
-payload = {'page': '0', 'isTNS_AT': 'yes', 'classified_sne': '1', 'num_page': '1000', 'format': 'csv'}
-
-www = 'https://wis-tns.weizmann.ac.il/search'
-
-r = requests.get(www, params=payload)
-print(r.url)
-
-pagenum = 0
-
-out = r.text.split('\n')
-headers = np.array([val.replace('"', '') for val in out[0].split(',')])
-ncols = len(headers)
-datatype = np.array([headers.dtype]*ncols) # for use in table initiation
-datatype[headers=='redshift'] = 'float'
-datatype[headers=='ID'] = 'int'
-nrows = len(out[1:])
-
-
-# Fill in the table
-t = Table(names=headers, dtype=[headers.dtype] * ncols)
-while nrows > 0:
-    for rownum in np.arange(1,nrows-1):
-        row = np.array([val.replace('"', '') 
-                        for val in out[rownum].split(',')]).astype(str)
-        t.add_row(row)
-    pagenum += 1
+def set_payload(pagenum):
+    """ Set the payload """
     payload = {'page': '%s' %pagenum, 
                'isTNS_AT': 'yes', 
                'classified_sne': '1', 
                'num_page': '1000', 
                'format': 'csv'}
-    r = requests.get(www, params=payload)
-    print(r.url) 
+    return payload
+
+
+def run_query(pagenum):
+    www = 'https://wis-tns.weizmann.ac.il/search'
+    r = requests.get(www, params=set_payload(pagenum))
+    print(r.url)
     out = r.text.split('\n')
+    return out
+    
+
+def make_table():
+    pagenum = 0
+    out = run_query(pagenum)
+    headers = np.array([val.replace('"', '') for val in out[0].split(',')])
+    ncols = len(headers)
+    datatype = np.array([headers.dtype]*ncols) # for use in table initiation
     nrows = len(out[1:])
+
+    # Fill in the table
+    t = Table(names=headers, dtype=datatype)
+    while nrows > 0:
+        for rownum in np.arange(1,nrows-1):
+            row = np.array([val.replace('"', '') 
+                            for val in out[rownum].split(',')], dtype=np.string)
+            t.add_row(row)
+        pagenum += 1
+        out = run_query(pagenum)
+        nrows = len(out[1:])
+
+    t.write("tns_query_output.csv", format='csv', overwrite=True)
+
+
+if __name__=="__main__":
+    make_table()
+    #dat = Table.read("tns_query_output.csv", format="csv")
+    #redshift = list(filter(None, dat['redshift']))
 
